d8fcdf47b3594d493f1781dba4fe0af7a70e0148
kubitus2 <jakubosinski2@gmail.com>
2021-08-31 21:40:24 +0200

    Gravity added

--- GravityBalls/Assets/Scripts/Attractor.cs
+++ GravityBalls/Assets/Scripts/Attractor.cs
@@ -4,14 +4,38 @@ using UnityEngine;
 
 public class Attractor : MonoBehaviour
 {
-    const float G = 667f;
-    private Rigidbody rb;
+    const float G = 66.7f;
+    public Rigidbody rb;
     private static List<Attractor> Attractors;
 
 
-    // Update is called once per frame
-    void Update()
+    void Awake()
     {
-        
+        rb = this.gameObject.GetComponent<Rigidbody>();
+    }
+    void Attract (Attractor objToAttract)
+    {
+        Rigidbody rbToAttract = objToAttract.rb;
+        Vector3 direction = rb.position - rbToAttract.position;
+        float distance = direction.magnitude;
+
+        float magnitude = G * (rb.mass * rbToAttract.mass) / Mathf.Pow(distance, 2);
+        Vector3 force = direction.normalized * magnitude;
+
+        rbToAttract.AddForce(force);
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Attractor[] attractors = FindObjectsOfType<Attractor>();
+
+        if (attractors.Length > 1)
+        {
+            foreach (Attractor a in attractors)
+            {
+                if(a != this)
+                Attract(a);
+            }
+        }
     }
 }
