bbea826e93abb68a04b2188cb80fe8f963106f54
kubitus2 <jakubosinski2@gmail.com>
2021-09-01 21:58:44 +0200

    Moved camera utils to separate static class

--- GravityBalls/Assets/Scripts/BallSpawner.cs
+++ GravityBalls/Assets/Scripts/BallSpawner.cs
@@ -11,24 +11,18 @@ public class BallSpawner : MonoBehaviour
 
     [SerializeField]
     [Range(2, 250)]
-    private int maxNumberOfBalls = 50;
+    private int maxNumberOfBalls = 250;
 
     [SerializeField]
     private GameObject prefab;
     [SerializeField]
     private float intervalBetweenSpawns;
 
-    [SerializeField]
-    private float maxDistFromCamera = 20;
-
-    private Camera mainCamera;
-
     bool doInstantiate = true;
 
     void Awake()
     {
-        mainCamera = Camera.main;
-        mainCamera.farClipPlane = 4f * maxDistFromCamera;
+        Camera.main.farClipPlane = 1000f;
     }
 
     void Start()
@@ -36,35 +30,6 @@ public class BallSpawner : MonoBehaviour
         numOfBalls = 0;
         StartCoroutine(SpawnBalls(intervalBetweenSpawns));
     }
-    
-    Vector3 RandomPointInFrustum()
-    {
-        float randZ = Random.Range(mainCamera.nearClipPlane * 4f, maxDistFromCamera);
-        
-        return new Vector3(0f, 0f, randZ) + RandomPointOnFrustumPlane(randZ);
-    }
-
-    Vector3 RandomPointOnFrustumPlane(float dist)
-    {
-        float h = FrustumHeight(dist);
-        float w = FrustumWidth(h);
-
-        float x = Random.Range(-w/2, w/2);
-        float y = Random.Range(-h/2, h/2);
-
-        return new Vector3 (x, y, 0f);
-    }
-
-    float FrustumHeight(float dist)
-    {
-        return 2.0f * dist * Mathf.Tan(mainCamera.fieldOfView * 0.5f * Mathf.Deg2Rad);
-    }
-
-    float FrustumWidth(float height)
-    {
-        return height * mainCamera.aspect;
-    }
-
 
     IEnumerator SpawnBalls(float interval)
     {
@@ -72,7 +37,7 @@ public class BallSpawner : MonoBehaviour
         {
             numOfBalls++;
             Debug.Log(numOfBalls);
-            Instantiate(prefab, RandomPointInFrustum(), Quaternion.identity);
+            Instantiate(prefab, CameraUtilities.RandomPointInFrustum(), Quaternion.identity);
             yield return new WaitForSeconds(interval);
         }
     }
---
+++ GravityBalls/Assets/Scripts/CameraUtilities.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class CameraUtilities
+{
+
+    private static Camera mainCamera = Camera.main;
+    private static float maxDistFromCamera = 30;
+
+    public static Vector3 RandomPointInFrustum()
+    {
+        float randZ = Random.Range(mainCamera.nearClipPlane * 4f, maxDistFromCamera);
+        
+        return new Vector3(0f, 0f, randZ) + RandomPointOnFrustumPlane(randZ);
+    }
+
+    static Vector3 RandomPointOnFrustumPlane(float dist)
+    {
+        float h = FrustumHeight(dist);
+        float w = FrustumWidth(h);
+
+        float x = Random.Range(-w/2, w/2);
+        float y = Random.Range(-h/2, h/2);
+
+        return new Vector3 (x, y, 0f);
+    }
+
+    public static float FrustumHeight(float dist)
+    {
+        return 2.0f * dist * Mathf.Tan(mainCamera.fieldOfView * 0.5f * Mathf.Deg2Rad);
+    }
+
+    public static float FrustumWidth(float height)
+    {
+        return height * mainCamera.aspect;
+    }
+}
